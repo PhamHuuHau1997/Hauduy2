4fd316e56e6783d3fc558b4e4fa33cd9ef7f4761
Pham Hau <phamhuuhau.vip9a3@gmail.com>
2018-03-14 16:29:19 +0700

    BaiTap
    abc

---
+++ HauDuy1.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $str=" Chao Mung den voi Chanel cua minh : LapTrinhWEB";
+        echo $str= 'Lap trinh Web 1'."<br>";
+        echo nl2br('Chao <br>  Mung <br>  den <br> voi<br> Chanel<br> cua<br> minh :<br> LapTrinhWEB<br>');
+        ?>
+    </body>
+</html>
+
